ce7695e39383d281a345907f6005f4462443656f
aaren <aar.liu13@gmail.com>
2021-01-26 17:19:05 +1100

    update
    Signed-off-by: aaren <aar.liu13@gmail.com>

--- app1.py
+++ app1.py
@@ -3,14 +3,14 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(w):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
+            return data[get_close_matches(word, data.keys())[0]]
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
@@ -19,6 +19,7 @@ def translate(w):
         return "The word doesn't exist. Please double check it!!"
 
 word = input("Enter word: ")
+
 output = translate(word)
 if type(output) == list:
     for item in output:
